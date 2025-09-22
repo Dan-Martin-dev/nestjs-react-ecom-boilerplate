d52b59ae9a181b4a73560e4494e2f5db67a2699a
Vare <dm.va@hotmail.com>
2025-09-22 19:09:14 -0300

    fix:deleted sonner

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,12 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './globals.css'
-import { Toaster } from 'sonner'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <>
-      <Toaster />
       <App />
     </>
   </React.StrictMode>,
